be284d6970ca371b81883fc75442783a5e25471a
HoochDeveloper <michelesalvatore.rillo@gmail.com>
2015-10-10 12:30:01 +0200

    Aggiornata funzione distanza

--- Stickino_v0.1/Stickino_v0.1.ino
+++ Stickino_v0.1/Stickino_v0.1.ino
@@ -6,6 +6,9 @@
 #define PROBE_PIN 8 // sonar wave sensor PIN
 #define PROBE_RANGE 150 // relevant distance for the sensor ( in cm )
 
+// feedback calc. parameters
+#
+
 // init the Maxbotix to listen on the probe pin
 Maxbotix rangeSensorPW(PROBE_PIN, Maxbotix::PW, Maxbotix::LV);
 
@@ -38,7 +41,10 @@ void loop() {
 
 boolean sendingViber = false;
 unsigned long startViber = 0;
+unsigned long endViber = 0;
 int durationViber = 0;
+int durationPause = 0;
+
 
 /*
  * send a feedback to the  user based on the obstacle distance
@@ -47,30 +53,60 @@ int durationViber = 0;
 void sendVibeFeed(int distanceCm){
   #ifdef DEBUG
     Serial.print("\t\t<<\t - INPUT distance - \t>> ");
-    Serial.print(distanceCm);
+    Serial.println(distanceCm);
   #endif 
   // if ther is an obstacle in range
   if (distanceCm < 150){
     // check id the feedback has been sent
     if(sendingViber == false){
+      durationPause = (int(distanceCm / 5) + 1) * 50 ;
+    }else{
+      if(distanceCm < 50){
+        durationViber = (int(distanceCm / 5) + 1) * 50;
+      }else{
+        durationViber = (int(distanceCm / 5) + 1) * 25;
+      }
+    }
+    if(sendingViber == false && (millis() - endViber) > durationPause){
       // if not
-      // set up the state
+      // set up the state 
+      endViber = 0;
       startViber = millis();
       digitalWrite(VIBE_PIN, HIGH);
       sendingViber = true;
-      durationViber = (int(distanceCm / 5) + 1) * 25;
+       // evey 5cm the viber duration goes up of 25ms
+      durationPause = 0;
+
+      #ifdef DEBUG
+        Serial.print("\t\t<<\t - Start viber for - \t>> ");
+        Serial.println(durationViber);
+      #endif
+      
     }else if(sendingViber == true){
       // if the feedback has been sent
       // check if the feed must be terminated
-      if(millis() - startViber < durationViber){
-        digitalWrite(VIBE_PIN, LOW);
-        sendingViber = false;
+      if(millis() - startViber > durationViber){
+          endViber = millis();
+          startViber = 0;
+          digitalWrite(VIBE_PIN, LOW);
+          sendingViber = false;
+          durationViber = 0;
+          #ifdef DEBUG
+            Serial.print("\t\t<<\t - Stop viber for - \t>> "); Serial.println(durationPause);
+          #endif
       }
     }    
   }else{
     // if no obstacle in range reset the state
     digitalWrite(VIBE_PIN, LOW);
     sendingViber = false;
+    startViber = 0;
+    endViber = 0;
+    durationViber = 0;
+    durationPause = 0;
+    #ifdef DEBUG
+      Serial.println("\t\t<<\t - Stop viber no obstavle- \t>> ");  
+    #endif
   }
 }
 
